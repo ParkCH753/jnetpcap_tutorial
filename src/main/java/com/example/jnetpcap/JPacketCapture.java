512045f13a749f16f8b1de451f5f0fabfd70161b
minjun <ijmtylhtf@naver.com>
2023-05-07 22:24:17 +0900

    [Feat] #1 - 네이버 캡쳐 패킷 이용 usedTime 계산

--- src/main/java/com/example/jnetpcap/JPacketCapture.java
+++ src/main/java/com/example/jnetpcap/JPacketCapture.java
@@ -1,5 +1,6 @@
 package com.example.jnetpcap;
 
+import org.jnetpcap.JCaptureHeader;
 import org.jnetpcap.Pcap;
 import org.jnetpcap.PcapBpfProgram;
 import org.jnetpcap.PcapIf;
@@ -9,8 +10,10 @@ import org.jnetpcap.protocol.network.Ip4;
 import org.jnetpcap.protocol.tcpip.Tcp;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,8 +23,8 @@ public class JPacketCapture {
 
     private Pcap pcap;
 
-    public JPacketCapture() {
-        // 네트워크 인터페이스 목록 조회
+    public JPacketCapture() throws IOException {
+
         List<PcapIf> allDevs = new ArrayList<>();
         StringBuilder errbuf = new StringBuilder();
         if (Pcap.findAllDevs(allDevs, errbuf) != Pcap.OK) {
@@ -33,8 +36,20 @@ public class JPacketCapture {
             return;
         }
 
-        // 캡처할 인터페이스 선택
+        // 네트워크 인터페이스 목록 조회
+        int interfaceNum=0;
+        for (PcapIf device : allDevs) {
+            System.out.printf("[%d번]: %s [%s]\n ",interfaceNum++,device.getName(),device.getDescription());
+            System.out.println("device.getAddresses() = " + device.getAddresses());
+            System.out.println("device.getHardwareAddress() = " + device.getHardwareAddress());
+        }
+
+        //캡처할 인터페이스 선택
         PcapIf pcapIf = allDevs.get(1);
+        System.out.println("pcap = " + pcapIf.getDescription());
+        System.out.println("pcapIf.getName() = " + pcapIf.getName());
+        System.out.println("pcapIf.getAddresses() = " + pcapIf.getAddresses());
+        System.out.println("pcapIf.getHardwareAddress() = " + pcapIf.getHardwareAddress());
 
         // 패킷 캡처 시작
         pcap = Pcap.openLive(pcapIf.getName(), 65536, Pcap.MODE_PROMISCUOUS, 1000, errbuf);
@@ -46,6 +61,7 @@ public class JPacketCapture {
     public void captureNaverPacket() {
 
 
+        //네이버 도메인 관련 필터 생성
         String filter = "tcp port 443";
         PcapBpfProgram program = new PcapBpfProgram();
         int mask = 0xffffff;
@@ -60,6 +76,7 @@ public class JPacketCapture {
 
         try {
             InetAddress address = InetAddress.getByName("www.naver.com");
+            //네이버의 IP 주소 223.130.195.95
             addrNaver = address.getHostAddress();
 
             System.out.println("네이버의 IP 주소: " + addrNaver);
@@ -69,35 +86,69 @@ public class JPacketCapture {
         }
 
         // 도메인 IP 고정된 부분 추출하기
+        //223.130
         String regex = "^(\\d{1,3}\\.\\d{1,3})\\..*";
         String extractedAddr = null;
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(addrNaver);
+        System.out.println("pattern = " + pattern);
+        System.out.println("matcher = " + matcher);
 
         if (matcher.matches()) { extractedAddr = matcher.group(1); }
         System.out.println();
 
         // 캡처된 패킷 처리
+        //finalExtractedAddr : 223.130
         String finalExtractedAddr = extractedAddr;
+        System.out.println("finalExtractedAddr = " + finalExtractedAddr);
+
         JPacketHandler<String> packetHandler = new JPacketHandler<>() {
             private final Tcp tcp = new Tcp();
             private final Ip4 ip = new Ip4();
 
+            int naverPacketNum=0; //naver 필터에 부합하는 몇 번째 필터인지
+
+            long firstPacketCapturedTime=0;//첫 네이버 패킷의 캡쳐된 시간
             @Override
             public void nextPacket(JPacket packet, String user) {
                 if (packet.hasHeader(tcp) && packet.hasHeader(ip)) {
 
+                    System.out.printf("캡처 시작: %s\n 패킷의 길이: %-4d\n", new Date(packet.getCaptureHeader().timestampInMillis()),
+                            packet.getCaptureHeader().caplen());
+
+                    //System.out.println("packet.toString() = " + packet.toString());
                     String srcIp = org.jnetpcap.packet.format.FormatUtils.ip(ip.source());
                     String dstIp = org.jnetpcap.packet.format.FormatUtils.ip(ip.destination());
-//                    System.out.println("Source IP: " + srcIp);
-//                    System.out.println("Destination IP: " + dstIp);
-
+                    System.out.println("Source IP: " + srcIp);
+                    System.out.println("Destination IP: " + dstIp);
+                    //네이버로 오고가는 패킷 캡쳐 및 추출
                     if (srcIp.startsWith(finalExtractedAddr) || dstIp.startsWith(finalExtractedAddr)) {
 
-                        byte[] data = packet.getByteArray(tcp.getOffset(), tcp.getHeaderLength() + tcp.getPayloadLength());
+                        long usedTime; //사용시간 (단위는 우선 밀리세컨드, 추후 수정 가능)
 
-//                    System.out.println(packet);
+                        //첫번째 패킷의 캡쳐 시간은 계속 저장해놓기 위해 변수에 저장
+                        if(naverPacketNum ==0){
+                            firstPacketCapturedTime = packet.getCaptureHeader().timestampInMillis();
+                            System.out.println("naver Packet First Captured");
+                            System.out.println("firstPacketCapturedTime = " + firstPacketCapturedTime);
+                            //naverPacketNum++;
+                        }
+                        //지속해서 네이버 패킷을 캡쳐한 후 첫번째 네이버 패킷 캡쳐 시간과의 차를 usedTime 변수에 저장
+                        else{
+                            System.out.println("not first Time Captured ");
+                            long currentCapturedPacket = packet.getCaptureHeader().timestampInMillis();
+                            System.out.println("currentCapturedPacket = " + currentCapturedPacket);
+                            usedTime = currentCapturedPacket -firstPacketCapturedTime;
+                            System.out.println("usedTime = " + usedTime);
+                            //naverPacketNum++;
+                        }
+                        naverPacketNum++; //캡쳐된 네이버 패킷 수 증가
+
+                        byte[] data = packet.getByteArray(tcp.getOffset(), tcp.getHeaderLength() + tcp.getPayloadLength());
+                        System.out.println("packet = " + packet);
+                        long time = packet.getCaptureHeader().timestampInMillis();
+
                         if (tcp.destination() == 443) {
                             // SSL/TLS 핸드셰이크 패킷 처리
                             String hex = bytesToHex(data);
@@ -190,7 +241,6 @@ public class JPacketCapture {
             @Override
             public void nextPacket(JPacket packet, String user) {
                 if (packet.hasHeader(tcp) && packet.hasHeader(ip)) {
-
                     byte[] data = packet.getByteArray(tcp.getOffset(), tcp.getHeaderLength() + tcp.getPayloadLength());
 
 
